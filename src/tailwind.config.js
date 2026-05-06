2e1577f1f687e0c2c542fa83977ea17ff9d4af40
MicaelLopes0 <micaell.loppes@gmail.com>
2026-05-06 20:05:48 -0300

    Adicionado app.jsx, styles.css e tailwind.config.js

---
+++ src/tailwind.config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        purple: {
+          DEFAULT: "#7B00FF",
+          light: "#9B40FF",
+        },
+        neon: "#00FF85",
+        dark: {
+          1: "#0D0D0D",
+          2: "#111111",
+          3: "#181818",
+          4: "#222222",
+        },
+      },
+    },
+  },
+};
